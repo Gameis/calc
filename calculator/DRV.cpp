b5103043a872fb0edacef49010d3d837948685d9
Max Pc <thegangamer@gmail.com>
2018-11-15 10:26:19 +0700

    Фикс ошибки 10 для ДСВ

--- calculator/DRV.cpp
+++ calculator/DRV.cpp
@@ -4,7 +4,10 @@ unsigned DRV::compute(const vector<double> & answer) {
 	this->initializeData(answer);
 	if (answer.size() < 4) return 11; // Кол-во X-ов должно быть >= "2"
 	else if (this->_sum != 1) return 13; // Вероятность < или > "1", должна быть = "1"
-	else if (this->isAlreadyX() != false) return 10; // В распределение ДСВ присутствуют одинаковые Х-ы
+	else if (this->isAlreadyX() != false) {
+		this->computeNumericalCharacteristics();
+		return 10; // В распределение ДСВ присутствуют одинаковые Х-ы
+	}
 	else {
 		this->computeNumericalCharacteristics();
 		return 0;
